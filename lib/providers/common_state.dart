1b5979ed3270b291f659b7f5eae4efe4a74067d5
Praveen-Aanand <pravinand.io@gmail.com>
2022-06-18 12:14:12 +0530

    current data model

--- lib/providers/common_state.dart
+++ lib/providers/common_state.dart
@@ -1,1 +1,13 @@
-import ""
+import 'package:flutter/widgets.dart';
+import "package:provider/provider.dart";
+import "../models/current_data.dart";
+
+class CommonState extends ChangeNotifier {
+  late CurrentData _currentData;
+
+  set currentData(CurrentData val) {
+    _currentData = val;
+  }
+
+  CurrentData get currentData => _currentData;
+}
